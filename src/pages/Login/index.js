aef0032ea0fb020f46fca493dd64308ab1978f56
Mysia14 <mysiasantana@gmail.com>
2022-04-04 15:10:48 -0600

    added login icons

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,29 @@
 import React from "react"
 import "./login.css"
+import { BiLock } from "react-icons/bi";
+import { BiMailSend } from "react-icons/bi";
+
 
 const Login = () => {
     return(
         <div className="login">
             <div className="login-logo">
                 <img src="../assets/images/login-icon.png" alt="Login logo"></img>
+
+            </div>
+            <div className="login-right">
+                <h1>Login</h1>
+
+                <div className="loginInputEmail">
+                <input type= "email" placeholder="E-mail Address"></input>
+                <BiMailSend/>
+                </div>
+                
+                <div>
+                <input type="password" placeholder="Password"></input>
+                <BiLock/>
+                
+                </div>
 
             </div>
         </div>
