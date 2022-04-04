332d70d6b89708d8418e3d6efe15a9e5d894525c
Mysia14 <mysiasantana@gmail.com>
2022-04-04 14:50:17 -0600

    added lgin icon.png

---
+++ src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./login.css"
+
+const Login = () => {
+    return(
+        <div className="login">
+            <div className="login-logo">
+                <img src="../assets/images/login-icon.png" alt="Login logo"></img>
+
+            </div>
+        </div>
+    )
+}
+export default Login;
