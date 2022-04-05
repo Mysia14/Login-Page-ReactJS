8a9a839877f35112f1943fefac93c37c682957cf
Mysia14 <mysiasantana@gmail.com>
2022-04-05 10:32:56 -0600

    added useState and more React icons

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,35 +1,43 @@
-import React from "react"
+import React,{useState} from "react"
 import "./login.css"
 import { BiLock } from "react-icons/bi";
 import { BiMailSend } from "react-icons/bi";
+import { HiOutlineEyeOff,HiEye } from "react-icons/hi";
 
 
-const Login = () => {
+function Login () {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState ("")
+    const [show, setShow] = useState (false)
     return(
         <div className="login">
             <div className="login-logo">
                 <img src="../assets/images/login-icon.png" alt="Login logo"></img>
             </div>
-
             <div className="login-right">
-                <h1>Login</h1>
+                <h1>Login App</h1>
                 <div className="login-input-email">
-                <BiMailSend/>
-                <input type= "email" placeholder="email@domain.com"></input>
+                    <BiMailSend/>
+                    <input type= "email" placeholder="email@domain.com" value={email} onChange={e =>setEmail(e.target.value)}  ></input>
+                </div>
+                <div className="login-input-password">
+                    <BiLock/>
+                    <input type= {show ? "text" : "password"}  placeholder= "Password" value={password} onChange={e =>setPassword(e.target.value)}></input>
+                        <div className="login-eye">
+                            {show ? (<HiEye/>) : (<HiOutlineEyeOff/>)}
+                        </div>
                 </div>
 
-                <div className="login-input-password">
-                <BiLock/>
-                <input type="password" placeholder="Password"></input>
-                </div>
-                <button  type="submit">
-                    Sign In
-                </button>
+                    <button  type="submit">
+                        <p>Sign In</p>
+                    </button>
 
                 <h4>Don't haver an account? </h4>
+
                 <button type="submit">
-                    Creat New Account
+                    <p>Creat New Account</p>
                 </button>
+
             </div>
         </div>
     )
